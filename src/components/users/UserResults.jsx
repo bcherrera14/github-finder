eb9c65009cecbeab243fa774766eea6bd0d47059
Bryan Herrera <bcherrera14@gmail.com>
2022-08-21 12:28:14 -0700

    add user profile

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import './UserItem';
+import UserItem from './UserItem';
 
 function UserResults() {
 	const [ users, setUsers ] = useState([]);
@@ -25,7 +27,7 @@ function UserResults() {
 	if (!loading) {
 		return (
 			<div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-				{users.map((user) => <h3>{user.login}</h3>)}
+				{users.map((user) => <UserItem key={user.id} user={user} />)}
 			</div>
 		);
 	} else {
