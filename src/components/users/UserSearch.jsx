a193c0af30b65025bb01d6fea2d6c91a8112591a
Bryan Herrera <bcherrera14@gmail.com>
2022-08-23 16:53:00 -0700

    add get user data to actions

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -9,7 +9,7 @@ function UserSearch() {
 
 	const handleChange = (e) => setText(e.target.value);
 
-	const { users, dispatch, clearUsers } = useContext(GithubContext);
+	const { users, dispatch } = useContext(GithubContext);
 
 	const { setAlert } = useContext(AlertContext);
 
@@ -50,7 +50,7 @@ function UserSearch() {
 			</div>
 			{users.length > 0 && (
 				<div>
-					<button onClick={clearUsers} className="btn btn-ghost btn-lg">
+					<button onClick={() => dispatch({ type: 'CLEAR_USERS' })} className="btn btn-ghost btn-lg">
 						Clear
 					</button>
 				</div>
