fad30a3dba35629f438b57000b7724b47cbd8802
Bryan Herrera <bcherrera14@gmail.com>
2022-08-22 18:23:21 -0700

    add repo item

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -107,28 +107,28 @@ function User() {
 				</div>
 				<div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
 					<div className="stat">
-						<div className="stat-figure text-info">
+						<div className="stat-figure text-primary">
 							<FaUsers className="text-3xl md:text-5xl" />
 						</div>
 						<div className="stat-title pr-5">Followers</div>
 						<div className="stat-value pr-5 text-3xl md:text-4xl">{followers}</div>
 					</div>
 					<div className="stat">
-						<div className="stat-figure text-info">
+						<div className="stat-figure text-primary">
 							<FaUserFriends className="text-3xl md:text-5xl" />
 						</div>
 						<div className="stat-title pr-5">Following</div>
 						<div className="stat-value pr-5 text-3xl md:text-4xl">{following}</div>
 					</div>
 					<div className="stat">
-						<div className="stat-figure text-info">
+						<div className="stat-figure text-primary">
 							<FaCode className="text-3xl md:text-5xl" />
 						</div>
 						<div className="stat-title pr-5">Public Repositories</div>
 						<div className="stat-value pr-5 text-3xl md:text-4xl">{public_repos}</div>
 					</div>
 					<div className="stat">
-						<div className="stat-figure text-info">
+						<div className="stat-figure text-primary">
 							<FaStore className="text-3xl md:text-5xl" />
 						</div>
 						<div className="stat-title pr-5">Public Gists</div>
